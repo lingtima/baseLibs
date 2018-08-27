424419127644dd86b3e442fdabaf3b7afd7e7e5a
lingtima@gmail.com <lingtima@gmail.com>
2018-08-27 03:56:32 +0000

    增加无限极分类的处理

---
+++ src/Services/Common.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/8/27
+ * Time: 9:25
+ */
+
+namespace Tools;
+
+class Common
+{
+    /**
+     * 将无限极分类的list转化成tree
+     *
+     * @param array $result 保存结果数组
+     * @param array $origin 原始list数组
+     * @param \Closure|null $cbFormat 格式化数据
+     * @param string $cNodeName 保存子节点的键
+     */
+    public function listToTree(&$result, $origin, \Closure $cbFormat = null, $cNodeName = 'children')
+    {
+        $parentFlagName = 'id';
+        $flagName = 'pid';
+
+        $initNode = false;
+        if (empty($result)) {
+            $initNode = true;
+        }
+
+        foreach ($origin as $k => $v) {
+            if ($v[$flagName] == ($initNode ? 0 : $result[$parentFlagName])) {
+                if ($initNode) {
+                    $result[] = $cbFormat ? $cbFormat($v) : $v;
+                } else {
+                    $result[$cNodeName][] = $cbFormat ? $cbFormat($v) : $v;
+                }
+                unset($origin[$k]);
+            }
+        }
+
+        if (!empty($origin && !empty($result))) {
+            foreach ($result as $k => &$v) {
+                if (is_array($v)) {
+                    $this->listToTree($v, $origin, $cbFormat, $cNodeName);
+                }
+            }
+        }
+    }
+}
--- test.php
+++ test.php
@@ -4,43 +4,143 @@
  * Time: 2018-04-09 15:01
  */
 
-function baseConvert($numberInput, $fromBaseInput = '0123456789', $toBaseInput = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+function listToTree(&$result, $origin, \Closure $cbFormat = null, $cNodeName = 'children')
 {
-    if ($fromBaseInput === $toBaseInput) {
-        return $numberInput;
+    $parentFlagName = 'id';
+    $flagName = 'pid';
+
+    $initNode = false;
+    if (empty($result)) {
+        $initNode = true;
     }
-    $fromBase = str_split($fromBaseInput, 1);
-    $toBase = str_split($toBaseInput, 1);
-    $number = str_split($numberInput, 1);
-    
-    $fromLen = strlen($fromBaseInput);
-    $toLen = strlen($toBaseInput);
-    $numberLen = strlen($numberInput);
-    
-    $retVal = '';
-    if ($toBaseInput === '0123456789') {
-        $retVal = 0;
-        for ($i = 1; $i <= $numberLen; $i++) {
-            $retVal = bcadd($retVal, bcmul(array_search($number[$i - 1], $fromBase, true), bcpow($fromLen, $numberLen - $i)));
+
+    foreach ($origin as $k => $v) {
+        if ($v[$flagName] == ($initNode ? 0 : $result[$parentFlagName])) {
+            if ($initNode) {
+                $result[] = $cbFormat ? $cbFormat($v) : $v;
+            } else {
+                $result[$cNodeName][] = $cbFormat ? $cbFormat($v) : $v;
+            }
+            unset($origin[$k]);
         }
-        
-        return $retVal;
     }
-    if ($fromBaseInput !== '0123456789') {
-        $base10 = baseConvert($numberInput, $fromBaseInput, '0123456789');
-    } else {
-        $base10 = $numberInput;
+
+    if (!empty($origin && !empty($result))) {
+        foreach ($result as $k => &$v) {
+            if (is_array($v)) {
+                listToTree($v, $origin, $cbFormat, $cNodeName);
+            }
+        }
     }
-    
-    if ($base10 < strlen($toBaseInput)) {
-        return $toBase[$base10];
-    }
-    
-    while ($base10 !== '0') {
-        $retVal = $toBase[bcmod($base10, $toLen)] . $retVal;
-        $base10 = bcdiv($base10, $toLen, 0);
-    }
-    return $retVal;
 }
 
-echo baseConvert('9999');
+$arr = [
+    [
+        'id' => 1,
+        'pid' => 0,
+    ],
+    [
+        'id' => 2,
+        'pid' => 15,
+    ],
+    [
+        'id' => 3,
+        'pid' => 1,
+    ],
+    [
+        'id' => 4,
+        'pid' => 18,
+    ],
+    [
+        'id' => 5,
+        'pid' => 1,
+    ],
+    [
+        'id' => 6,
+        'pid' => 2,
+    ],
+    [
+        'id' => 7,
+        'pid' => 2,
+    ],
+    [
+        'id' => 8,
+        'pid' => 1,
+    ],
+    [
+        'id' => 9,
+        'pid' => 1,
+    ],
+    [
+        'id' => 10,
+        'pid' => 1,
+    ],
+    [
+        'id' => 11,
+        'pid' => 1,
+    ],
+    [
+        'id' => 12,
+        'pid' => 1,
+    ],
+    [
+        'id' => 13,
+        'pid' => 3,
+    ],
+    [
+        'id' => 14,
+        'pid' => 4,
+    ],
+    [
+        'id' => 15,
+        'pid' => 4,
+    ],
+    [
+        'id' => 16,
+        'pid' => 4,
+    ],
+    [
+        'id' => 17,
+        'pid' => 1,
+    ],
+    [
+        'id' => 18,
+        'pid' => 2,
+    ],
+    [
+        'id' => 19,
+        'pid' => 5,
+    ],
+    [
+        'id' => 20,
+        'pid' => 3,
+    ],
+    [
+        'id' => 21,
+        'pid' => 0,
+    ],
+    [
+        'id' => 22,
+        'pid' => 12,
+    ],
+    [
+        'id' => 23,
+        'pid' => 20,
+    ],
+    [
+        'id' => 24,
+        'pid' => 23,
+    ],
+    [
+        'id' => 25,
+        'pid' => 24,
+    ],
+];
+
+$ret = [];
+
+listToTree($ret, $arr, function ($arr) {
+    return $arr;
+});
+
+var_dump($ret);
