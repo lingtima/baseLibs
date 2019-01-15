d0ff626e9b3cfec03562513da3cab818c515628a
anhjisjlyq <lingtima@gmail.com>
2019-01-15 14:00:14 +0800

    feat(redis):添加程序组件：访问频次限制、访问量、信号量

--- src/Services/Redis/Semaphore.php
+++ src/Services/Redis/Semaphore.php
@@ -10,7 +10,7 @@ use Carbon\Carbon;
 use Predis\Client as Redis;
 
 /**
- * 计数信号量控制
+ * 计数信号量控制-通常用于限制一种资源最多能够同时被多少进程访问。如下的是对宽泛信号量的应用
  * @package Tools\Services\Redis
  */
 class Semaphore
@@ -20,6 +20,7 @@ class Semaphore
 
     const EXPIRATION_FOR_REDIS_TYPE_STRING = 86400 * 90;//string key 的默认过期时间
     const EXPIRATION_FOR_ACCESSES_LIMIT = 60 * 5;//访问频次控制的默认过期时间
+    const EXPIRATION_FOR_ACCESSES_LIMIT_OF_SIMPLE = 60 * 2;//访问频次控制的默认过期时间
 
     const MAX_NUMBER_FOR_ACCESSES_LIMIT          = 20;//单位时间内最大访问次数
     const SECONDS_FOR_CHECK_GAP                  = 60;//检查访问频次的时间间隔
@@ -60,5 +61,91 @@ class Semaphore
                 throw new \Exception('error');
             }
         }
+    }
+
+    /**
+     * 按分钟统计访问频次
+     */
+    public static function getAccessesLimitSimple()
+    {
+        $nowTimestamp = Carbon::now()->timestamp;
+        $accessesLimitRedisKey = self::getRedisKeyForAccessesLimit($nowTimestamp);
+        Redis::set($accessesLimitRedisKey, 0, 'EX', self::EXPIRATION_FOR_ACCESSES_LIMIT_OF_SIMPLE, 'NX');
+        Redis::incr($accessesLimitRedisKey, 1);
+    }
+
+    /**
+     * 每分钟访问量
+     */
+    public static function setAccessesLimitWithList()
+    {
+        $now = Carbon::now();
+        $ymd = $now->format('Ymd');
+        $minutes = $now->minute;
+
+        $accessesLimitRedisKey = self::getRedisKeyForAccessesLimit($ymd);
+        if (!Redis::exists($accessesLimitRedisKey)) {
+            Redis::hincrby($accessesLimitRedisKey, $minutes, 1);
+            Redis::expire($accessesLimitRedisKey, 86400 * 7);
+        } else {
+            Redis::hincrby($accessesLimitRedisKey, $minutes, 1);
+        }
+    }
+
+    /**
+     * 普通信号量
+     */
+    public static function acquireSemaphore()
+    {
+        $key = 'key';
+        $expirationSeconds = 20;
+        $identifier = uniqid();
+        $timestamp = Carbon::now()->getPreciseTimestamp();
+        $expirationTime = $timestamp - $expirationSeconds;
+        $maxLimit = 20;
+
+        Redis::zremrangebyscore($key, '-inf', $expirationTime);
+        Redis::zadd($key, $timestamp, $identifier);
+
+        if (Redis::zrevrank($key, $identifier) + 1 > $maxLimit) {
+            //超过了最大信号量
+            Redis::zrem($key, $identifier);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 公平信号量（增加一个计数器、一个超时信号量集合）
+     */
+    public static function acquireFairSemaphore()
+    {
+        $counterKey = 'counter_key';
+        $normalKey = 'normal_key';
+        $expirationKey = 'expiration_key';
+
+        $identifier = uniqid();
+        $expirationSeconds = 20;
+        $nowTimestamp = Carbon::now()->timestamp;
+        $maxLimit = 20;
+
+        Redis::zremrangebyscore($expirationKey, '-inf', $nowTimestamp - $expirationSeconds);//删除超时信号量
+        Redis::zinterstore($normalKey, [$normalKey, $expirationKey], []);//取交集
+
+        $counter = Redis::incr($counterKey);//计数器自增
+        Redis::zadd($normalKey, $counter, $identifier);
+        Redis::zadd($expirationKey, $nowTimestamp, $identifier);
+
+        if (Redis::zrevrank($normalKey, $identifier) + 1 > $maxLimit) {//判断是否超过了最大信号量
+            //超过了最大信号量
+            Redis::zrem($normalKey, $identifier);
+            Redis::zrem($expirationKey, $identifier);
+            return false;
+        } else {
+            return true;
+        }
+
+
     }
 }
